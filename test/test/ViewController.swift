a9255f9635f5d075fc198c2971001214f24e45c0
tjlpow <tjlevil@gmail.com>
2016-06-07 12:35:59 +0800

    second

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    let button = 1
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
